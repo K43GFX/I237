8990fe48d2c1782c06bfaf068cf7b25a5c553284
Karl Erik <karl.ounapuu@itcollege.ee>
2017-11-08 16:23:08 +0200

    Lab 3 code

--- src/main.c
+++ src/main.c
@@ -1,32 +1,13 @@
+#include <stdio.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include "uart.h"
 
 #define BLINK_DELAY_MS 300
 
-void blink_red(void)
-{
-    PORTA |= _BV(PORTA0);
-    _delay_ms(BLINK_DELAY_MS);
-    PORTA &= ~_BV(PORTA0);
-    _delay_ms(BLINK_DELAY_MS);
-}
-
-void blink_green(void)
-{
-    PORTA |= _BV(PORTA2);
-    _delay_ms(BLINK_DELAY_MS);
-    PORTA &= ~_BV(PORTA2);
-    _delay_ms(BLINK_DELAY_MS);
-}
-
-void blink_blue(void)
-{
-    PORTA |= _BV(PORTA4);
-    _delay_ms(BLINK_DELAY_MS);
-    PORTA &= ~_BV(PORTA4);
-    _delay_ms(BLINK_DELAY_MS);
-}
-void main (void)
+static inline void init_leds(void)
 {
     /* Set port A pin 7 for output*/
     DDRB |= _BV(DDB7);
@@ -36,11 +17,64 @@ void main (void)
     DDRA |= _BV(DDA0);
     DDRA |= _BV(DDA2);
     DDRA |= _BV(DDA4);
+}
+
+
+/* Init error console as stderr in UART1 and print user code info */
+static inline void init_errcon(void)
+{
+    simple_uart1_init();
+    stderr = &simple_uart1_out;
+    fprintf(stderr, "Version: %s built on: %s %s\n",
+            FW_VERSION, __DATE__, __TIME__);
+    fprintf(stderr, "avr-libc version: %s avr-gcc version: %s\n",
+            __AVR_LIBC_VERSION_STRING__, __VERSION__);
+}
+
+
+static inline void blink_leds(void)
+{
+    /* Not putting into cycle, otherwise wouldn't proceed with main() */
+    PORTA |= _BV(PORTA0);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~_BV(PORTA0);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA |= _BV(PORTA2);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~_BV(PORTA2);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA |= _BV(PORTA4);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~_BV(PORTA4);
+    _delay_ms(BLINK_DELAY_MS);
+}
+
+
+void main(void)
+{
+    init_leds();
+    init_errcon();
+    /* Test assert - REMOVE IN FUTURE LABS */
+    char *array;
+    uint32_t i = 1;
+    extern int __heap_start, *__brkval;
+    int v;
+    array = malloc( i * sizeof(char));
+    assert(array);
+    /* End test assert */
 
     while (1) {
-        /* Play with colors */
-        blink_red();
-        blink_green();
-        blink_blue();
+        blink_leds();
+        /* Test assert - REMOVE IN FUTURE LABS */
+        /*
+         * Increase memory allocated for array by 100 chars
+         * until we have eaten it all and print space between stack and heap.
+         * That is how assert works in run-time.
+         */
+        array = realloc( array, (i++ * 1000) * sizeof(char));
+        fprintf(stderr, "%d\n",
+                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
+        assert(array);
+        /* End test assert */
     }
 }
